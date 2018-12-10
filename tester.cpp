bf613d1dc3ac994b50334f003381e5b05ea674ef
Michael Quinn <mquinn5@binghamton.edu>
2018-12-10 16:41:29 -0500

    added files

---
+++ tester.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+	bool run = true;
+	while(run){
+		
+	}
+	return 0;
+}
